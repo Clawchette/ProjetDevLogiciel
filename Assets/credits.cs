23adb45021029e653fc9402d998cae3da8d907a3
Clawchette <marie.gonzalez@ynov.com>
2020-05-11 18:35:23 +0200

    merge???
    le premier a pas marché

---
+++ Assets/credits.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class credits : MonoBehaviour
+{
+    public TextMeshProUGUI credit_count;
+    public int compteur;
+    // Start is called before the first frame update
+    void Start()
+    {
+        compteur = 0;
+        credit_count.text = "credit:" + compteur;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        credit_count.text = "credit:" + compteur;
+    }
+}
