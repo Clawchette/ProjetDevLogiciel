6632f352ab00b36892ac94035cbaf66902e42712
tankplasma <adrien.alard@gmail.com>
2020-05-11 15:25:03 +0200

    modif de la vitesse

--- Assets/player_move.cs
+++ Assets/player_move.cs
@@ -8,7 +8,7 @@ public class player_move : MonoBehaviour
 
     // Start is called before the first frame update
     void Start(){
-        speed=10.0f;
+        speed=5.0f;
     }
 
     // Update is called once per frame
