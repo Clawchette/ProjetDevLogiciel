b73b15ee44c6db1876c3ee8aed521f8f0786c552
Clawchette <marie.gonzalez@ynov.com>
2020-05-25 11:17:05 +0200

    fix

--- Assets/change_stat_square.cs
+++ Assets/change_stat_square.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class change_stat_square : MonoBehaviour
 {
-    public void DeletSpeed()
+    public void MonterDeplacement()
     {
         GameObject.Find("gameManager").GetComponent<GameManager>().vitesseDeplacement --;
     }
-    public void UpgradeSpeed()
+    public void BaisserDeplacement()
     {
         GameObject.Find("gameManager").GetComponent<GameManager>().vitesseDeplacement ++;
     }
-    public void DeletDefilement()
+    public void BaisserDefilement()
     {
         GameObject.Find("gameManager").GetComponent<GameManager>().vitesseDefilement --;
     }
-    public void UpgradeDefilement()
+    public void MonterDefilement()
     {
         GameObject.Find("gameManager").GetComponent<GameManager>().vitesseDefilement ++;
     }
-    public void DeletBulletSpeed()
+    public void BaisserTir()
     {
         GameObject.Find("gameManager").GetComponent<GameManager>().vitesseTir --;
     }
-    public void UpgradeBulletSpeed()
+    public void MonterTir()
     {
         GameObject.Find("gameManager").GetComponent<GameManager>().vitesseTir ++;
     }
