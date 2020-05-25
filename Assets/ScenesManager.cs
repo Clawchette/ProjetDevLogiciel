34c469aaac66c76629f89ada7522cf0ceb758ca2
Clawchette <marie.gonzalez@ynov.com>
2020-05-25 12:24:07 +0200

    script scenes manager

---
+++ Assets/ScenesManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    void Start(){
+        gameManager=gameObject.GetComponent<GameManager>();
+    }
+
+    public void StartGame(){
+        gameObject.GetComponent<Save>().LoadGame();
+        SceneManager.LoadScene("SampleScene");
+        gameManager.isGameActive=true;
+    }
+
+    public void RetourMenuPrincipal(){
+        SceneManager.LoadScene("menue_scene");
+    }
+
+
+}
