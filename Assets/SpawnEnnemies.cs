2499e42d6e089f3e2fe2848c4772f582276c6471
Clawchette <marie.gonzalez@ynov.com>
2020-05-31 19:46:26 +0200

    opti SpawnEnnemies.cs

--- Assets/SpawnEnnemies.cs
+++ Assets/SpawnEnnemies.cs
@@ -12,12 +12,10 @@ public class SpawnEnnemies : MonoBehaviour
     public GameObject Neutre;               //ennemi sans effet special
     private Vector3 position;
     private int randomPos;
-    private int spawnSpeed;                  //un nouvel ennemi spawn toutes les *spawnSpeed* secondes
     private float timer;
 
     void Start()
     {
-        spawnSpeed=3;
         gameManager=gameObject.GetComponent<GameManager>();
     }
 
@@ -34,24 +32,24 @@ public class SpawnEnnemies : MonoBehaviour
                 position = new Vector3(randomPos, 30, 0);
                 //lance la fonction qui permet de faire apparaitre l'ennemi
                 SpawnEnnemy(position);
-                timer = spawnSpeed;
+                timer = 3;
             }
         }
     }
 
     private void SpawnEnnemy(Vector3 position){
         int random = Random.Range(1, 100);         //proba de spawn des types d'ennemis
-        if (1 <= random && random < 55)            //55% - Ennemi neutre  
+        if (random < 55)            //55% - Ennemi neutre  
         {
             Instantiate(Neutre, position, gameObject.transform.rotation);
         }
-        else if(55 <= random && random < 70){      //15% - Ennemi qui aumente la vitesse de defilement
+        else if(random < 70){      //15% - Ennemi qui aumente la vitesse de defilement
             Instantiate(DefilementAugm, position, gameObject.transform.rotation);
         }
-        else if(70 <= random && random < 85){      //15% - Ennemi qui augmente la vitesse de deplacement
+        else if(random < 85){      //15% - Ennemi qui augmente la vitesse de deplacement
             Instantiate(DeplacementAugm, position, gameObject.transform.rotation);
         }
-        else if(85 <= random && random < 100){     //15% - Ennemi qui diminue la vitesse de tir
+        else if(random < 100){     //15% - Ennemi qui diminue la vitesse de tir
             Instantiate(VitTirDimin, position, gameObject.transform.rotation);
         }
     }
